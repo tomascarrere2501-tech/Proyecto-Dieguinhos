6cdc8499e9e4c58feb58c26408b9744870b8831e
johanan07 <johanan.contreras2502@alumnos.ubiobio.cl>
2026-04-07 16:44:29 -0400

    Add files via upload

---
+++ Pasaporte.java
@@ -0,0 +1,22 @@
+public class Pasaporte {
+
+    private String numero;
+    private String nacionalidad;
+
+    public Pasaporte(String num, String nacionalidad) {
+        this.numero = num;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public static Pasaporte of(String num, String nacionalidad) {
+        return new Pasaporte(num, nacionalidad);
+    }
+}
---
+++ Rut.java
@@ -0,0 +1,27 @@
+public class Rut {
+private int numero;
+private char dv;
+
+    private Rut(int numero, char dv){
+        this.numero= numero;
+        this.dv= dv;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public char getDv() {
+        return dv;
+    }
+
+    public static Rut of(String rutConDv){
+        String[] partes = rutConDv.split("-");
+
+        int numero = Integer.parseInt(partes[0]);
+        char dv = partes[1].charAt(0);
+
+        return new Rut(numero, dv);
+    }
+}
+
