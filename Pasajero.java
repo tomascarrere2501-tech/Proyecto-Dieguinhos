da9dffc3e94b8ed5cc3435dd286da3bfdab0113d
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-22 12:06:46 -0400

    Add files via upload

---
+++ Pasajero.java
@@ -0,0 +1,20 @@
+public class Pasajero {
+    private Nombre nomContacto;
+    private String fonoContacto;
+
+    public Nombre getNomContacto() {
+        return nomContacto;
+    }
+
+    public void setNomContacto(Nombre nomContacto) {
+        this.nomContacto = nomContacto;
+    }
+
+    public String getFonoContacto() {
+        return fonoContacto;
+    }
+
+    public void setFonoContacto(String fonoContacto) {
+        this.fonoContacto = fonoContacto;
+    }
+}
