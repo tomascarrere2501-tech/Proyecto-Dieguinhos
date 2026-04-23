a27b3c94c17dfcef8c2526af3afd6bbbbe5326be
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-22 22:59:59 -0400

    Update Pasajero.java

--- Pasajero.java
+++ Pasajero.java
@@ -1,6 +1,13 @@
-public class Pasajero {
+public class Pasajero extends Persona {
     private Nombre nomContacto;
     private String fonoContacto;
+
+
+    public Pasajero(IdPersona id, Nombre nombre, Nombre nomContacto, String fonoContacto) {
+        super(id, nombre); 
+        this.nomContacto = nomContacto;
+        this.fonoContacto = fonoContacto;
+    }
 
     public Nombre getNomContacto() {
         return nomContacto;
