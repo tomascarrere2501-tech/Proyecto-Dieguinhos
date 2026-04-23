54a60e4375bff245ad4d85bbe8c3e6fb11e50888
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-22 23:22:22 -0400

    Update Venta.java

--- Venta.java
+++ Venta.java
@@ -1,14 +1,24 @@
 import java.time.LocalDate;
-
+import java.util.Arraylist;
 public class Venta {
     private  String iddocumento;
     private tipo Tipodocummento;
     private LocalDate fecha;
 
-    public Venta ( tipo Tipodocumento, String iddocumento, LocalDate fecha){
+    private Cliente cliente;
+    private ArrayList<Pasaje> pasajesInternos;
+
+    public Venta ( tipo Tipodocumento, String iddocumento, LocalDate fecha , Cliente cliente ){
         this. iddocumento= iddocumento;
         this.fecha=fecha;
         this.Tipodocummento= Tipodocumento;
+        this.cliente = cliente;
+        this.pasajesInternos = new ArrayList<>();
+
+       // Asocia el cliente a la venta,ocupandose  objeto Cliente 
+        // agregue esta venta en su colección."
+
+        cli.addVenta(this);
 
     }
 
@@ -32,22 +42,26 @@ public class Venta {
 
     public void createPasaje( int asiento, Viaje viaje,Pasajero pasajero ){
 
-        System.out.println("Asiento " +  " " +  asiento);
-        System.out.println("Viaje" +  " " +  viaje);
-        System.out.println("Pasajero " +  " " +   pasajero);
+        // esto es para "Crea un objeto Pasaje, a partir de los datos recibidos  agregándolo a su colección"
+        // se  asume que el constructor de Pasaje recibe (int, Viaje, Pasajero, Venta) según el UML
+        
+        Pasaje nuevoPasaje = new Pasaje(asiento, viaje, pasajero, this);
+        this.pasajesInternos.add(nuevoPasaje);
 
 
     }
 
-     public Pasaje  getPasajes(){
-        return Pasajes ;
+     public Pasaje []  getPasajes(){
+        return pasajesInternos.toArray(new Pasaje[0]);
      }
 
      public int  getMonto(){
-        return Monto;
+        nt total = 0;
+    for (Pasaje p : pasajesInternos) {
+        // Obtenemos el viaje asociado al pasaje y sumamos su precio
+        total += p.getViaje().getPrecio();
+        
      }
-
-
-     
+     return total;
 
 }
