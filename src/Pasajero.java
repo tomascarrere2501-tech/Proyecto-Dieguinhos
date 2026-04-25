1f94b5cbba51caa177f01f1f993714a22fb6ae47
Francisco Alexander Fernández Chávez <francisco.fernandez2501@alumnos.ubiobio.cl>
2026-04-25 15:24:39 -0400

    Update Pasajero.java

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -2,22 +2,19 @@ public class Pasajero {
     //Atributos
     private Nombre nomContacto;
     private String fonoContacto;
-
-    //Constructor
-    public Pasajero(Nombre nomContacto, String fonoContacto){}
-    //Metodo getNomContacto
+    
     public Nombre getNomContacto() {
         return nomContacto;
-    }
-
+    } //Metodo getNomContacto
+    
     public void setNomContacto(Nombre nom) {
         this.nomContacto = nom;
     } //Metodo setNomContacto
     
     public String getFonoContacto() {
         return fonoContacto;
-    }  //Metodo getFonoContacto
-
+    } //Metodo getFonoContacto
+    
     public void setFonoContacto(String fono) {
         this.fonoContacto = fono;
     } //Metodo setFonoContacto
