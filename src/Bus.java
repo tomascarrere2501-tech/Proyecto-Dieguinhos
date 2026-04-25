585abd3b2ce017b1dd9e6231924574622d14540e
Francisco Alexander Fernández Chávez <francisco.fernandez2501@alumnos.ubiobio.cl>
2026-04-25 15:18:45 -0400

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class Bus {
+
+    //Atributos
+    private String patente;
+    private String marca;
+    private String modelo;
+    private int nroAsientos;
+    private ArrayList<Viaje> listaViajes;
+
+    public Bus(String patente, int nroAsientos) {
+        this.patente = patente;
+        this.nroAsientos = nroAsientos;
+        this.listaViajes = new ArrayList<>();
+    } //Constructor
+
+    public String getPatente() {
+        return patente;
+    } //Metodo getPatente
+
+    public String getMarca() {
+        return marca;
+    } //Metodo getMarca
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    } //Metodo setMarca
+
+    public String getModelo() {
+        return modelo;
+    } //Metodo getModelo
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    } //Metodo setModelo
+
+    public int getNroAsientos() {
+        return nroAsientos;
+    } //Metodo getNroAsientos
+
+    public void addViaje(Viaje viaje) {
+        this.listaViajes.add(viaje);
+    } //Metodo addViaje
 }
