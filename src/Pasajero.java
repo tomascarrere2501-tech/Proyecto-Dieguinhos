afb415be30c1285f3040e6fb9d51d01322889027
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-25 15:26:57 -0400

    Update Pasajero.java

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -1,21 +1,27 @@
-public class Pasajero {
-    //Atributos
+public class Pasajero extends Persona {
     private Nombre nomContacto;
     private String fonoContacto;
-    
+
+
+    public Pasajero(IdPersona id, Nombre nombre, Nombre nomContacto, String fonoContacto) {
+        super(id, nombre); 
+        this.nomContacto = nomContacto;
+        this.fonoContacto = fonoContacto;
+    }
+
     public Nombre getNomContacto() {
         return nomContacto;
-    } //Metodo getNomContacto
-    
-    public void setNomContacto(Nombre nom) {
-        this.nomContacto = nom;
-    } //Metodo setNomContacto
-    
+    }
+
+    public void setNomContacto(Nombre nomContacto) {
+        this.nomContacto = nomContacto;
+    }
+
     public String getFonoContacto() {
         return fonoContacto;
-    } //Metodo getFonoContacto
-    
-    public void setFonoContacto(String fono) {
-        this.fonoContacto = fono;
-    } //Metodo setFonoContacto
+    }
+
+    public void setFonoContacto(String fonoContacto) {
+        this.fonoContacto = fonoContacto;
+    }
 }
