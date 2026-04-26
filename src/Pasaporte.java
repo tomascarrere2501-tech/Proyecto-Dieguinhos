724b7f59732f1f714bf499195777250cbc6a8b4e
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-26 01:56:01 -0400

    Update Pasaporte.java

--- src/Pasaporte.java
+++ src/Pasaporte.java
@@ -1,5 +1,4 @@
-public class Pasaporte {
-
+public class Pasaporte implements IdPersona {
     private String numero;
     private String nacionalidad;
 
@@ -18,5 +17,18 @@ public class Pasaporte {
 
     public static Pasaporte of(String num, String nacionalidad) {
         return new Pasaporte(num, nacionalidad);
+    }
+
+    @Override
+    public String toString() {
+        return numero + " " + nacionalidad;
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        if (this == otro) return true;
+        if (otro == null || getClass() != otro.getClass()) return false;
+        Pasaporte pasaporte = (Pasaporte) otro;
+        return this.numero.equals(pasaporte.numero) && this.nacionalidad.equals(pasaporte.nacionalidad);
     }
 }
