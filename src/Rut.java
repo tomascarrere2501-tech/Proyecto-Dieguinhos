b38ef80c9c35596f3b51b624dd3a5c4ad03a2e70
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-26 01:58:07 -0400

    Refactor Rut class and implement IdPersona
    Refactored Rut class to implement IdPersona interface and added error handling in of() method.

--- src/Rut.java
+++ src/Rut.java
@@ -1,8 +1,8 @@
-public class Rut {
+public class Rut implements IdPersona {
     private int numero;
     private char dv;
 
-    private Rut(int numero, char dv){
+    public Rut(int numero, char dv){
         this.numero= numero;
         this.dv= dv;
     }
@@ -16,11 +16,33 @@ public class Rut {
     }
 
     public static Rut of(String rutConDv){
-        String[] partes = rutConDv.split("-");
+        try {
+            String[] partes = rutConDv.split("-");
 
-        int numero = Integer.parseInt(partes[0]);
-        char dv = partes[1].charAt(0);
 
-        return new Rut(numero, dv);
+            String numeroSinPuntos = partes[0].replace(".", "");
+
+            int numero = Integer.parseInt(numeroSinPuntos);
+            char dv = partes[1].charAt(0);
+
+            return new Rut(numero, dv);
+        } catch (Exception e) {
+
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,d-%c", numero, dv).replace(',', '.');
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        if (this == otro) return true;
+        if (otro == null || getClass() != otro.getClass()) return false;
+
+        Rut rutOtro = (Rut) otro;
+        return this.numero == rutOtro.numero && this.dv == rutOtro.dv;
     }
 }
