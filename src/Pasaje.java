5cdde9bbdc112298ebe806856a393dff20d1a523
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-26 01:54:24 -0400

    Update Pasaje.java

--- src/Pasaje.java
+++ src/Pasaje.java
@@ -5,28 +5,33 @@ public class Pasaje {
     private Pasajero pasajero;
     private Venta venta;
 
-    public Pasaje (int asiento, Viaje viaje, Pasajero pasajero, Venta venta){
-     this.asiento=asiento;
-     this.viaje=viaje;
-     this.pasajero=pasajero;
-     this.venta=venta;
-     this.numero = System.currentTimeMillis();
+    public Pasaje(int asiento, Viaje viaje, Pasajero pasajero, Venta venta) {
+        this.asiento = asiento;
+        this.viaje = viaje;
+        this.pasajero = pasajero;
+        this.venta = venta;
+        this.numero = System.currentTimeMillis();
 
+        viaje.addPasaje(this);
     }
-    public long getNumero(){
-        return numero;
+
+    public int getNumero() {
+        return (int) numero;
     }
-    public long getAsiento(){
+
+    public int getAsiento() {
         return asiento;
     }
-    public Viaje getViaje (){
+
+    public Viaje getViaje() {
         return viaje;
     }
-    public Pasajero getPasajero (){
+
+    public Pasajero getPasajero() {
         return pasajero;
     }
-    public Venta getVenta (){
+
+    public Venta getVenta() {
         return venta;
     }
-
 }
