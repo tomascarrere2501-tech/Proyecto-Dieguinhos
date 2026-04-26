5cb8f7fb14ef7494ecda5b56e64c7d64fa7e09f4
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-04-26 01:57:31 -0400

    Update Persona.java

--- src/Persona.java
+++ src/Persona.java
@@ -1,43 +1,44 @@
-import java.util.Objects;
-
 public class Persona {
-    private IdPersona idPersona ;
-    private Nombre nombreCompleto ;
+    private IdPersona idPersona;
+    private Nombre nombreCompleto;
     private String telefono;
-    public Persona(IdPersona id, Nombre nombre){
-        this.idPersona= id;
-        this.nombreCompleto= nombre;
+
+    public Persona(IdPersona id, Nombre nombre) {
+        this.idPersona = id;
+        this.nombreCompleto = nombre;
     }
 
     public IdPersona getIdPersona() {
         return idPersona;
     }
-    public Nombre getNombreCompleto(){
-        return nombreCompleto;
 
+    public Nombre getNombreCompleto() {
+        return nombreCompleto;
     }
 
-    public void setNombreComleto(Nombre nombreCompleto){
-        this.nombreCompleto=nombreCompleto;
+    // modificacion aqui codigo anterior faltaba una p en el nombre del metodo 
+    public void setNombreCompleto(Nombre nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
     }
 
     public String getTelefono() {
         return telefono;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Persona persona = (Persona) o;
-        return Objects.equals(idPersona, persona.idPersona) && Objects.equals(nombreCompleto, persona.nombreCompleto) && Objects.equals(telefono, persona.telefono);
+    public String toString() {
+        return idPersona.toString() + ", " + nombreCompleto.toString() + ", " + telefono;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(idPersona, nombreCompleto, telefono);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return this.idPersona.equals(persona.idPersona);
     }
 }
-//corregido por tomasCarrere
