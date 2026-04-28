c9cddd34bf9bbd6a47d6e1ca467444537b9f4dc2
Francisco Alexander Fernández Chávez <francisco.fernandez2501@alumnos.ubiobio.cl>
2026-04-27 21:04:04 -0400

    Update Viaje.java

--- src/Viaje.java
+++ src/Viaje.java
@@ -3,7 +3,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Viaje {
-    //Atributos
     private LocalDate fecha;
     private LocalTime hora;
     private int precio;
@@ -16,51 +15,61 @@ public class Viaje {
         this.precio = precio;
         this.bus = bus;
         this.listaPasajes = new ArrayList<>();
-    } //Constructor
 
-    public LocalDate getFecha() {
-        return this.fecha;
-    } //Metodo getFecha
+        bus.addViaje(this);
+    }
 
-    public LocalTime getHora() {
-        return this.hora;
-    } //Metodo getHora
-
-    public int getPrecio() {
-        return this.precio;
-    } //Metodo getPrecio
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    } //Metodo setPrecio
-
-    public Bus getBus() {
-        return this.bus;
-    } //Metodo getBus
+    public LocalDate getFecha() { return this.fecha; }
+    public LocalTime getHora() { return this.hora; }
+    public int getPrecio() { return this.precio; }
+    public void setPrecio(int precio) { this.precio = precio; }
+    public Bus getBus() { return this.bus; }
 
     public void addPasaje(Pasaje pasaje) {
         this.listaPasajes.add(pasaje);
-    } //Metodo addPasaje
+    }
 
     public int getNroAsientosDisponibles() {
         if (this.bus != null) {
             return this.bus.getNroAsientos() - this.listaPasajes.size();
         }
         return 0;
-    } //Metodo getNroAsientosDisponibles
+    }
 
     public boolean existeDisponibilidad() {
-        if (this.getNroAsientosDisponibles() > 0) {
-            return true;
-        }
-        return false;
-    } //Metodo existeDisponibilidad
+        return this.getNroAsientosDisponibles() > 0;
+    }
 
     public String[][] getListaPasajeros() {
-        return new String[0][0];
-    } //getListaPasajeros
+        // CORRECCIÓN: Según PDF pág 14, deben ser 4 columnas (ID, nombre, nombre contacto, teléfono contacto)
+        String[][] matrizPasajeros = new String[this.listaPasajes.size()][4];
+        for (int i = 0; i < this.listaPasajes.size(); i++) {
+            Pasaje p = this.listaPasajes.get(i);
+            matrizPasajeros[i][0] = p.getPasajero().getIdPersona().toString();
+            matrizPasajeros[i][1] = p.getPasajero().getNombreCompleto().toString();
+            matrizPasajeros[i][2] = p.getPasajero().getNomContacto().toString();
+            matrizPasajeros[i][3] = p.getPasajero().getFonoContacto();
+        }
+        return matrizPasajeros;
+    }
 
     public String[][] getAsientos() {
-        return new String[0][0];
-    } //getAsientos
+        int totalAsientos = this.bus.getNroAsientos();
+        String[][] asientos = new String[totalAsientos][2];
+
+        for (int i = 0; i < totalAsientos; i++) {
+            int numeroAsiento = i + 1;
+            asientos[i][0] = String.valueOf(numeroAsiento);
+
+            boolean ocupado = false;
+            for (Pasaje p : listaPasajes) {
+                if (p.getAsiento() == numeroAsiento) {
+                    ocupado = true;
+                    break;
+                }
+            }
+            asientos[i][1] = ocupado ? "Ocupado" : "Libre";
+        }
+        return asientos;
+    }
 }
