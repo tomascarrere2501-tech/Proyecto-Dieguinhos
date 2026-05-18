0b3bba52c2546297fcf702dff18a996467f435ba
tomas010309 <tomas.carrere2501@alumnos.ubiobio.cl>
2026-05-17 23:00:37 -0400

    Add files via upload

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,31 @@
+package modelo;
+import utilidades.IdPersona;
+import utilidades.Nombre;
+import java.util.ArrayList;
+
+public class Cliente extends Persona {
+    private String email;
+    private ArrayList<Venta> ventasInternas;
+
+    public Cliente(IdPersona id, Nombre nom, String email) {
+        super(id, nom);
+        this.email = email;
+        this.ventasInternas = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void addVenta(Venta venta) {
+        this.ventasInternas.add(venta);
+    }
+
+    public Venta[] getVentas() {
+        return this.ventasInternas.toArray(new Venta[0]);
+    }
+}
