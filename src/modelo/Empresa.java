61c08ff3c86ccca2a18804f95334935dff70692f
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-05-24 20:39:58 -0400

    Update Empresa.java

--- src/modelo/Empresa.java
+++ src/modelo/Empresa.java
@@ -1,26 +1,28 @@
 package modelo;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.HashSet;
-import java.util.Set;
+
 import utilidades.IdPersona;
 import utilidades.Nombre;
 import utilidades.Rut;
+import utilidades.Direccion;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Empresa {
-    private Rut rut ;
+    private Rut rut;
     private String nombre;
     private String url;
     private List<Bus> buses;
     private List<Tripulante> tripulantes;
 
-    public Empresa(Rut rut , String nombre, String url ){
+
+    public Empresa(Rut rut, String nombre) {
         this.rut = rut;
         this.nombre = nombre;
-        this.url = url;
 
+        this.buses = new ArrayList<>();
+        this.tripulantes = new ArrayList<>();
     }
 
     public Rut getRut() {
@@ -39,55 +41,48 @@ public class Empresa {
         this.url = url;
     }
 
- public void addBus( Bus bus){
-    this.Buses.add(bus);
- }
-
-    public Optional<Bus> getBus(String patente) {
-        for (Bus b : buses) {
-            if (b.getPatente().equalsIgnoreCase(patente)) {
-                return Optional.of(b);
-            }
-        }
-        return Optional.empty();
+    public void addBus(Bus bus) {
+        this.buses.add(bus);
     }
 
-
- public  Bus[] getBuses(){
+    public Bus[] getBuses() {
         return this.buses.toArray(new Bus[0]);
- }
+    }
 
-
-
- public boolean addConductor(IdPersona id , Nombre nom , Direccion direccion){
-        for( Tripulante t : tripulantes){
-            if (t.getIdPersona().equals(id)){
+    public boolean addConductor(IdPersona id, Nombre nom, Direccion dir) {
+        for (Tripulante t : tripulantes) {
+            if (t.getIdPersona().equals(id)) {
                 return false;
             }
         }
+        this.tripulantes.add(new Conductor(id, nom, dir));
+        return true;
+    }
 
-     public boolean addAuxiliar(IdPersona id, Nombre nom, Direccion direccion) {
-         for (Tripulante t : tripulantes) {
-             if (t.getIdPersona().equals(id)) return false; // Ya existe
-         }
-         this.tripulantes.add(new Auxiliar(id, nom, direccion));
-         return true;
- }
-     public Tripulante[] getTripulantes() {
-         return this.tripulantes.toArray(new Tripulante[0]);
-     }
+    public boolean addAuxiliar(IdPersona id, Nombre nom, Direccion dir) {
+        for (Tripulante t : tripulantes) {
+            if (t.getIdPersona().equals(id)) {
+                return false;
+            }
+        }
+        this.tripulantes.add(new Auxiliar(id, nom, dir));
+        return true;
+    }
 
-     public Venta[] getVentas() {
-         Set<Venta> ventasUnicas = new HashSet<>();
-         for (Bus b : buses) {
-             for (Viaje v : b.getViajes()) {
-                 for (Venta venta : v.getVentas()) {
-                     ventasUnicas.add(venta);
-                 }
-             }
-         }
-         return ventasUnicas.toArray(new Venta[0]);
-     }    }
+    public Tripulante[] getTripulantes() {
+        return this.tripulantes.toArray(new Tripulante[0]);
+    }
 
 
+    public Venta[] getVentas() {
+        Set<Venta> ventasUnicas = new HashSet<>();
+        for (Bus b : buses) {
+            for (Viaje v : b.getViajes()) {
+                for (Venta venta : v.getVentas()) {
+                    ventasUnicas.add(venta);
+                }
+            }
+        }
+        return ventasUnicas.toArray(new Venta[0]);
+    }
 }
