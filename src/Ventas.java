939a4728ea2510cc396325b205d5c9a33fa9eeec
Francisco Alexander Fernández Chávez <francisco.fernandez2501@alumnos.ubiobio.cl>
2026-05-25 13:40:51 -0400

    Create Ventas.java

---
+++ src/Ventas.java
@@ -0,0 +1,93 @@
+package modelo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Venta {
+
+    private String idDocumento;
+    private TipoDocumento tipo;
+    private LocalDate fecha;
+    private Cliente cliente;
+    private ArrayList<Pasaje> pasajes;
+
+    private Pago pago;
+
+    public Venta(String idDocumento, TipoDocumento tipo, LocalDate fecha, Cliente cliente) {
+        this.idDocumento = idDocumento;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.pasajes = new ArrayList<>();
+        this.pago = null;
+
+        cliente.addVenta(this);
+    }
+
+    public String getIdDocumento() {
+        return idDocumento;
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void createPasaje(int asiento, Viaje viaje, Pasajero pasajero) {
+        Pasaje nuevoPasaje = new Pasaje(asiento, viaje, pasajero, this);
+        this.pasajes.add(nuevoPasaje);
+    }
+
+    public Pasaje[] getPasajes() {
+        return pasajes.toArray(new Pasaje[0]);
+    }
+
+    public int getMonto() {
+        int total = 0;
+        for (Pasaje p : pasajes) {
+            total += p.getViaje().getPrecio();
+        }
+        return total;
+    }
+
+    public boolean pagaMonto() {
+        if (this.pago != null) {
+            return false; // Ya fue pagada
+        }
+        this.pago = new PagoEfectivo(this.getMonto());
+        return true;
+    }
+
+    public boolean pagaMonto(long numeroTarjeta) {
+        if (this.pago != null) {
+            return false; // Ya fue pagada
+        }
+        this.pago = new PagoTarjeta(this.getMonto(), numeroTarjeta);
+        return true;
+    }
+
+    public int getMontoPagado() {
+        if (this.pago != null) {
+            return this.pago.getMonto();
+        }
+        return 0;
+    }
+
+    public String getTipoPago() {
+        if (this.pago != null) {
+            if (this.pago instanceof PagoEfectivo) {
+                return "Efectivo";
+            } else if (this.pago instanceof PagoTarjeta) {
+                return "Tarjeta";
+            }
+        }
+        return null;
+    }
+}
