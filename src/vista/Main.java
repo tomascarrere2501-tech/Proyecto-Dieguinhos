09d6a983cf4eb7141ac8ee356ceb18bbf91da2e9
aaronmerino2401-a11y <aaron.merino2401@alumnos.ubiobio.cl>
2026-06-23 15:56:08 -0400

    Update Main.java

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -1,11 +1,7 @@
 package vista;
 
-import controlador.SistemaVentaPasajes;
-
 public class Main {
     public static void main(String[] args) {
-        SistemaVentaPasajes sistema = SistemaVentaPasajes.getInstance();
-        UISVP interfaz = new UISVP(sistema);
-        interfaz.menu();
+        UISVP.getInstance().menu();
     }
 }
